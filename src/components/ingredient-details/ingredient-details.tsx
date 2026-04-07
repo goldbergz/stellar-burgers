2e0f13c24c0c3b68f28cfea43f486de6e6f01a3b
goldbergz <zolotogornaya22@mail.ru>
2026-04-07 11:28:30 +0300

    feat: add slice for get ingredients

--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -1,13 +1,36 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { Preloader } from '../ui/preloader';
 import { IngredientDetailsUI } from '../ui/ingredient-details';
+import { useDispatch, useSelector } from '../../services/store';
+import {
+  getIngredients,
+  selectError,
+  selectIngredientById,
+  selectIsLoading
+} from 'src/services/slice';
+import { useParams } from 'react-router-dom';
 
 export const IngredientDetails: FC = () => {
   /** TODO: взять переменную из стора */
-  const ingredientData = null;
+  const dispatch = useDispatch();
+  const { id } = useParams<{ id: string }>();
 
-  if (!ingredientData) {
+  const ingredientData = useSelector(
+    id ? selectIngredientById(id) : () => null
+  );
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(getIngredients());
+  }, [dispatch]);
+
+  if (isLoading || !ingredientData) {
     return <Preloader />;
+  }
+
+  if (!isLoading && error) {
+    return <p>Запрос ингридиентов завершился с ошибкой: {error}</p>;
   }
 
   return <IngredientDetailsUI ingredientData={ingredientData} />;
---
+++ src/services/slice.ts
@@ -0,0 +1,67 @@
+import { getIngredientsApi } from '@api';
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { TIngredient } from '@utils-types';
+import { RootState } from './store';
+
+interface ingredientsState {
+  ingredients: TIngredient[];
+  isLoading: boolean;
+  error: string | null;
+}
+
+const initialState: ingredientsState = {
+  ingredients: [],
+  isLoading: true,
+  error: null
+};
+
+export const getIngredients = createAsyncThunk(
+  'ingridients/getAll',
+  async () => {
+    return await getIngredientsApi();
+  }
+);
+
+export const ingredientsSlice = createSlice({
+  name: 'ingredients',
+  initialState,
+  reducers: {},
+  selectors: {
+    selectIngredients: (sliceState) => {
+      return sliceState.ingredients;
+    },
+
+    selectIsLoading: (sliceState) => {
+      return sliceState.isLoading;
+    },
+    selectError: (sliceState) => {
+      return sliceState.error;
+    }
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getIngredients.pending, (state: ingredientsState) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(getIngredients.rejected, (state: ingredientsState, action) => {
+        state.isLoading = false;
+        state.error = action.error.message || 'Error';
+      })
+      .addCase(
+        getIngredients.fulfilled,
+        (state: ingredientsState, action: PayloadAction<TIngredient[]>) => {
+          state.isLoading = false;
+          state.ingredients = action.payload;
+        }
+      );
+  }
+});
+
+export default ingredientsSlice.reducer;
+
+export const { selectIngredients, selectIsLoading, selectError } =
+  ingredientsSlice.selectors;
+
+export const selectIngredientById = (id: string) => (state: RootState) =>
+  state.ingredients.ingredients.find((item) => item._id === id);
